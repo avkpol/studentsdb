1411c58ffaa16a5817dc19f5f65e3b993bd72232
Andriy <andriykrav@gmail.com>
2015-04-30 14:49:39 +0300

    added students_edit form p357

--- students/views/students.py
+++ students/views/students.py
@@ -6,7 +6,23 @@ from django.core.urlresolvers import reverse
 from ..models.students import Student
 from ..models.groups import Group
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-from datetime import datetime
+from datetime import datetime  
+from django.views.generic import UpdateView
+
+class StudentUpdateView(UpdateView):
+	model= Student
+	template_name = 'students/students_edit.html' 
+	
+	def get_success_url(self):
+		return u'%s?status_message= Студента успішно збережено!'% reverse('home')
+	def post(self, request, *args, **kwargs):
+		if request.POST.get(cancel_button):
+			return HttpResponseRedirect(
+				u'%s?status_message=Редагування студента відмінено!'
+				% reverse('home'))
+		else:
+			return super(StudentUpdateView, self).post(request, *args, **kwargs)
+		
 
 
 def students_list(request):
