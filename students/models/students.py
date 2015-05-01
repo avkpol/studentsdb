55e94eaa4fad5cddc156b95c068ce0fa18db747a
Andriy <andriykrav@gmail.com>
2015-05-01 07:46:54 +0300

    student_edit  with crispy-form p361

--- students/models/students.py
+++ students/models/students.py
@@ -11,28 +11,28 @@ class Student(models.Model):
 	first_name= models.CharField(
 		max_length=256,
 		blank= False,
-		verbose_name=u"Name")
+		verbose_name=u"Ім'я")
 	last_name = models.CharField(
 		max_length=256,
 		blank= False,
-		verbose_name=u"Surname")
+		verbose_name=u"Призвище")
 	middle_name=models.CharField(
 		max_length=256,
 		blank=True,
-		verbose_name= u"Middle name",
+		verbose_name= u"По-батькові",
 		default ='')             
  	birthday=models.DateField(
 		blank=False,
-		verbose_name = u"date of Birth",
+		verbose_name = u"Дата народження",
 		null=True)
 	photo= models.ImageField(
 		blank=True,
-		verbose_name=u"Photo",
+		verbose_name=u"Фото",
 		null=True)
 	ticket=models.CharField(
 		max_length=256,
 		blank=False,
-		verbose_name=u"Ticket")
+		verbose_name=u"Білет")
 	student_group=models.ForeignKey('Group',
 	    verbose_name=u"Група",
 	    blank=False,
@@ -40,7 +40,7 @@ class Student(models.Model):
 	    on_delete=models.PROTECT)
 	notes =models.TextField(
 		blank=True,
-		verbose_name=u"Remarks")
+		verbose_name=u"Нотатки")
 	
 	def __unicode__(self):
 		return u"%s %s" % (self.first_name, self.last_name)
